f7904561df2dc3b3daed5ccb723b34e0edac3ece
basebandit <evansonmwangi83@gmail.com>
2018-06-21 17:05:10 +0300

    Root Vue instance

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <v-app>
+      <app-header></app-header>
+      <v-content>
+        <v-container fluid>
+          <img src="./assets/app.png">
+          <router-view/>
+        </v-container>
+      </v-content>
+      <app-footer></app-footer>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/Header'
+import AppFooter from '@/components/Footer'
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>

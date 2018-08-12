8dbb49c22c316ba06cebd036c99aae729d76252a
basebandit <evansonmwangi83@gmail.com>
2018-08-12 23:05:38 +0300

    Added props to memory route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Stats from '@/components/Stats'
+import Memory from '@/components/Memory'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'Stats',
       component: Stats
+    },
+    {
+      path: '/event/:title',
+      name: 'Memory',
+      component: Memory,
+      props: true
     }
   ]
 })
